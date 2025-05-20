2464bd437102b90a2096a267e0a14273844ef605
diamond_cz <9718114+diamond-cz@user.noreply.gitee.com>
2025-05-20 15:24:35 +0800

    优化打印指定目录的目录结构函数

--- test/test_dirprint.py
+++ test/test_dirprint.py
@@ -29,7 +29,7 @@ def print_directory_structure(start_path, padding="", exclude_extensions=None, e
             print(f"{padding}{'└──> ' if is_last else '├──> '}{entry}")
             
             # 递归打印子文件夹的内容
-            new_padding = padding + ("    " if is_last else "│   ")
+            new_padding = padding + ("    " if is_last else "│    ")
             print_directory_structure(full_path, new_padding, exclude_extensions, exclude_folders)
         else:
             # 如果是文件，检查扩展名是否符合过滤条件
@@ -59,22 +59,7 @@ def print_directory_structure_plus(start_path, padding="", exclude_extensions=No
     folders = [entry for entry in entries if os.path.isdir(os.path.join(start_path, entry))]
     files = [entry for entry in entries if os.path.isfile(os.path.join(start_path, entry))]
 
-    # 先处理文件
-    for i, file in enumerate(files):
-        full_path = os.path.join(start_path, file)
-        
-        # 检查扩展名是否符合过滤条件
-        if exclude_extensions and any(full_path.endswith(ext) for ext in exclude_extensions):
-            continue
-        
-        # 判断是否是最后一个条目
-        is_last = i == len(files) - 1
-
-        # 打印文件,先处理的不需要用└──
-        # print(f"{padding}{'└── ' if is_last else '├── '}{file}")
-        print(f"{padding}{'├── ' if is_last else '├── '}{file}")
-
-    # 后处理文件夹
+    # 先打印文件夹
     for i, folder in enumerate(folders):
         full_path = os.path.join(start_path, folder)
         
@@ -86,25 +71,36 @@ def print_directory_structure_plus(start_path, padding="", exclude_extensions=No
         is_last = i == len(folders) - 1
         
         # 打印文件夹
-        print(f"{padding}{'└──> ' if is_last else '├──> '}{folder}")
+        print(f"{padding}{'├──> ' if is_last else '├──> '}{folder}")
         
         # 递归打印子文件夹的内容
-        new_padding = padding + ("    " if is_last else "│   ")
-        print_directory_structure(full_path, new_padding, exclude_extensions, exclude_folders)
+        new_padding = padding + ("    " if is_last else "│    ")
+        print_directory_structure(full_path, new_padding, exclude_extensions[1], exclude_folders)
+
+    # 后打印文件
+    for i, file in enumerate(files):
+        full_path = os.path.join(start_path, file)
+        
+        # 检查扩展名是否符合过滤条件
+        if exclude_extensions[0] and any(full_path.endswith(ext) for ext in exclude_extensions[0]):
+            continue
+        
+        # 判断是否是最后一个条目
+        is_last = i == len(files) - 1
+
+        # 打印文件
+        print(f"{padding}{'└── ' if is_last else '├── '}{file}")
+        
 
 
-
-
-# 示例调用
+# 打印指定文件夹的目录结构
 if __name__ == "__main__":
-    # folder_path = input("请输入文件夹路径: ")
-    # exclude_extensions_input = input("请输入要排除的文件扩展名（用逗号分隔，如 .txt,.jpg）: ")
-    # exclude_folders_input = input("请输入要排除的文件夹名（用逗号分隔，如 subfolder1,subfolder2）: ")
+    # exclude_extensions_input = input("请输入文件夹路径: ")
     exclude_extensions_input = "D:\Image_process\hiviewer"
-    exclude_extensions = [".py", ".png", ".json", "icon", ".exe", ".lng", ".db", ".dll",".ico",".jpg",".ini",".svg",".gif",".ttf"]
+    exclude_files = [".py", ".md", ".ini", ".png", ".json", "icon", ".exe", ".lng", ".db", ".dll",".ico",".jpg",".svg",".gif",".ttf"]   # 二级目录排除的文件
+    exclude_files_plus = [".png", ".json", "icon", ".exe", ".lng", ".db", ".dll",".ico",".jpg",".svg",".gif",".ttf"]     # 一级目录要排除的文件
     exclude_folders = ["__pycache__",".git","jpegr_lossless_rotator"]
 
     print(f"\nhiviewer")
-    # print_directory_structure(exclude_extensions_input, exclude_extensions=exclude_extensions, exclude_folders=exclude_folders)
-    print_directory_structure_plus(exclude_extensions_input, exclude_extensions=exclude_extensions, exclude_folders=exclude_folders)
+    print_directory_structure_plus(exclude_extensions_input, exclude_extensions=[exclude_files_plus,exclude_files], exclude_folders=exclude_folders)
 
